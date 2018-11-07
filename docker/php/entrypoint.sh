1d1dab3a9ec6b924ae2d5eacce6bec94938f6332
Thomas Talbot <thomas.talbot@fauya.fr>
2018-11-07 11:10:56 +0100

    update docker build

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -8,9 +8,14 @@ fi
 
 if [[ "$1" = 'php-fpm' ]] || [[ "$1" = 'bin/console' ]]; then
     # The first time volumes are mounted, the project needs to be recreated
-    if [[ "$APP_ENV" != 'prod' ]]; then
+    if [[ "$BUILD_ENV" != 'prod' ]]; then
         # Always try to reinstall deps when not in prod
-        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+        if [[ -x "$(command -v composer)" ]]; then
+            composer install --prefer-dist --no-progress --no-suggest --no-interaction
+        else
+            >&2 echo "Error : composer is not installed."
+            exit 1
+        fi
     fi
 
 	# Permissions hack because setfacl does not work on Mac and Windows
