7d9cbde9ba4821202e28196b894203b55182d9aa
Thomas Talbot <thomas.talbot@fauya.fr>
2018-11-11 18:43:51 +0100

    fix csrf fleet upload form

--- src/Infrastructure/Form/FleetUploadForm.php
+++ src/Infrastructure/Form/FleetUploadForm.php
@@ -11,20 +11,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FleetUploadForm extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('handleSC', TextType::class, [
-            ])
-            ->add('fleetFile', FileType::class, [
-            ]);
+            ->add('handleSC', TextType::class, [])
+            ->add('fleetFile', FileType::class, []);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => FleetUpload::class,
             'allow_extra_fields' => true,
+            'csrf_protection' => false,
         ]);
     }
 }
