83917cd31f40144730ba2bd4b24070aa27dc4582
Thomas Talbot <thomas.talbot@sensiolabs.com>
2019-05-14 22:25:30 +0200

    add sanitize handle when URL given

--- src/Form/LinkAccountForm.php
+++ src/Form/LinkAccountForm.php
@@ -6,6 +6,8 @@ use App\Form\Dto\LinkAccount;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LinkAccountForm extends AbstractType
@@ -13,6 +15,16 @@ class LinkAccountForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('handleSC', TextType::class, []);
+
+        // listener to sanitize a the handle if someone sets his RSI profile URL
+        $builder->get('handleSC')->addEventListener(FormEvents::PRE_SUBMIT, static function (FormEvent $event): void {
+            $handle = $event->getData();
+
+            if (strpos($handle, 'robertsspaceindustries.com') !== false) {
+                $parts = explode('/', $handle);
+                $event->setData(end($parts));
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
