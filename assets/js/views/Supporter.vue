4b2af0202caeb22b3f6d445e56c696564c46d4da
Thomas Talbot <thomas.talbot@fauya.fr>
2019-12-15 10:45:23 +0100

    add message if no backings

--- assets/js/views/Supporter.vue
+++ assets/js/views/Supporter.vue
@@ -34,6 +34,9 @@
                         <div v-if="monthlySpinner" class="mb-2 text-center">
                             <b-spinner variant="primary"></b-spinner>
                         </div>
+                        <b-alert v-if="monthlyUsers.length === 0" variant="warning" :show="monthlyUsers.length === 0" class="text-center">
+                            Nobody has backed this month yet. 😢 What if you would <a href="/supporters">support us</a>? 😎
+                        </b-alert>
                         <b-row v-for="user in monthlyUsers" :key="user.name" class="font-xl" :class="{'font-weight-bold': !!user.me}">
                             <b-col col class="text-right pr-0">{{ user.rank }}.</b-col>
                             <b-col col>{{ user.name }}</b-col>
@@ -45,6 +48,9 @@
                         <div v-if="alltimeSpinner" class="mb-2 text-center">
                             <b-spinner variant="primary"></b-spinner>
                         </div>
+                        <b-alert v-if="allTimeUsers.length === 0" variant="warning" :show="allTimeUsers.length === 0" class="text-center">
+                            Nobody has backed yet. 😢 What if you would <a href="/supporters">support us</a>? 😎
+                        </b-alert>
                         <b-row v-for="user in allTimeUsers" :key="user.name" class="font-xl" :class="{'font-weight-bold': !!user.me}">
                             <b-col col class="text-right pr-0">{{ user.rank }}.</b-col>
                             <b-col col>{{ user.name }}</b-col>
