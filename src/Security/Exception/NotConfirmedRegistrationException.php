30c99f4400ee6803bc32d8aac9f13533903520e8
Thomas Talbot <thomas.talbot@fauya.fr>
2019-12-18 09:05:11 +0100

    add fields in NotConfirmedRegistrationException

--- src/Security/Exception/NotConfirmedRegistrationException.php
+++ src/Security/Exception/NotConfirmedRegistrationException.php
@@ -2,10 +2,22 @@
 
 namespace App\Security\Exception;
 
+use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Throwable;
 
 class NotConfirmedRegistrationException extends AccountStatusException
 {
+    public UuidInterface $userId;
+    public string $username;
+
+    public function __construct(UuidInterface $userId, string $username, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->userId = clone $userId;
+        $this->username = $username;
+    }
+
     public function getMessageKey(): string
     {
         return 'not_confirmed_registration';
