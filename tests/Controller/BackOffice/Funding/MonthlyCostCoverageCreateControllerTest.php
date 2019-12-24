35faec9d772f674be38e5f0d551067e24c5cac9a
Thomas Talbot <thomas.talbot@fauya.fr>
2019-12-24 12:24:53 +0100

    add tests for BO pages

---
+++ tests/Controller/BackOffice/Funding/MonthlyCostCoverageCreateControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Controller\BackOffice\Funding;
+
+use App\Entity\User;
+use App\Tests\WebTestCase;
+
+class MonthlyCostCoverageCreateControllerTest extends WebTestCase
+{
+    /**
+     * @group functional
+     * @group funding
+     * @group bo
+     */
+    public function testNotAuth(): void
+    {
+        $this->client->request('GET', '/bo/monthly-cost-coverage/create');
+
+        $this->assertSame(401, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @group functional
+     * @group funding
+     * @group bo
+     */
+    public function testNotAdmin(): void
+    {
+        /** @var User $user */
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['username' => 'Gardien1']); // ROLE_USER
+        $this->logIn($user);
+        $this->client->request('GET', '/bo/monthly-cost-coverage/create');
+
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @group functional
+     * @group funding
+     * @group bo
+     */
+    public function testAdmin(): void
+    {
+        /** @var User $user */
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['username' => 'Ioni']); // ROLE_ADMIN
+        $this->logIn($user);
+        $this->client->request('GET', '/bo/monthly-cost-coverage/create');
+
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+}
