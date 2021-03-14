fec997936a3dc188961b548b789f811d3ef3c19c
Thomas Talbot <thomas.talbot@fauya.fr>
2021-03-14 13:17:02 +0100

    remove SPA related

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -6,10 +6,4 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-	# Permissions hack because setfacl does not work on Mac and Windows
-	mkdir -p var
-#	chown -R www-data var
-fi
-
 exec "$@"
