1fa68e118a4391ee4baab8d7e4a3c3af9a934069
Thomas Talbot <thomas.talbot@fauya.fr>
2021-03-16 20:36:27 +0100

    add nelmio CORS

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Auth0\JWTAuthBundle\JWTAuthBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
